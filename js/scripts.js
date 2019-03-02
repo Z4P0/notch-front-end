b2bc7e752fa63d96dd10865b58d9ca583133609f
luis rosario <lrosario101@gmail.com>
2019-03-02 11:28:17 -0500

    add some simple interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,28 @@ ClassicEditor
     .catch( error => {
         // console.error( error );
     } );
+
+
+var $newPostModal = document.querySelector('#new-post-modal');
+var $newContentModal = document.querySelector('#new-content-modal');
+
+
+document.querySelector('#new-post-button').addEventListener('click', function(event) {
+    $newPostModal.classList.toggle('hide');
+});
+
+document.querySelector('#new-post-modal--open-new-post-button').addEventListener('click', function(event) {
+    console.log($newContentModal);
+    $newContentModal.classList.toggle('hide');
+});
+
+
+
+
+document.querySelector('#new-post-modal__close-buton').addEventListener('click', function(event) {
+    $newPostModal.classList.toggle('hide');
+});
+
+document.querySelector('#close-button--new-content-modal').addEventListener('click', function(event) {
+    $newContentModal.classList.toggle('hide');
+});
